9ce8b4383eea05ecc3c8981a9e08823164412b3d
Ojas Sharma <ojassharma16@gmail.com>
2024-09-02 23:45:01 -0400

    commit-2

---
+++ indexing.py
@@ -0,0 +1,26 @@
+from elasticsearch import Elasticsearch
+from pymongo import MongoClient
+
+# Connect to MongoDB
+client = MongoClient('mongodb://localhost:27017/')
+db = client['financial_news']
+collection = db['articles']
+
+# Connect to Elasticsearch
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+
+def index_articles():
+    articles = collection.find({})
+    for article in articles:
+        # Prepare the document to be indexed
+        doc = {
+            'title': article['title'],
+            'content': article['cleaned_content'],
+            'date': article.get('date', 'N/A')
+        }
+        # Index the document in Elasticsearch
+        es.index(index='financial_news', id=str(article['_id']), body=doc)
+        print(f"Indexed article: {article['_id']}")
+
+if __name__ == "__main__":
+    index_articles()
