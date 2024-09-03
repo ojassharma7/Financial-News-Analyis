21bc571f4655d0ad298a7bb62f3ce1159f27013d
Ojas Sharma <ojassharma16@gmail.com>
2024-09-03 15:39:09 -0400

    lastest commit

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template
+ffrom flask import Flask, render_template, request
+from summarization import generate_summary
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, welcome to the Financial News Summarizer!
-PAR IS LOVELY"
+    return render_template('index.html')
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    query = request.form['query']
+    summary = generate_summary(query)
+    return render_template('result.html', summary=summary)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
